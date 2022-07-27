83f6e34cf5686b1f701ceb899039c32fc424aadb
destiny1831 <110090831+destiny1831@users.noreply.github.com>
2022-07-27 13:39:16 +0530

    Add files via upload

---
+++ taskthree.java
@@ -0,0 +1,23 @@
+package assignments;
+
+public class taskthree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =14;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==n-1||((j==0||j==n-1)&&i>(n-1)/2)||(i-(n/2)+1>j-1)||(i-(n/2)+1>n-2-j)) 
+				System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
